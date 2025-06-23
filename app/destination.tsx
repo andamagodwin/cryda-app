3a74ee7bc31f6a3881fca9d012befd27766beb23
andamagodwin <andamagodwinezra@gmail.com>
2025-06-23 23:25:58 +0300

    Add LocationSelector component for location search and selection

--- app/destination.tsx
+++ app/destination.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
+import LocationSelector from "~/components/Location/LocationPicker";
 
 
 export default function Destination() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-lg font-bold text-gray-800">Destination Screen</Text>
+      <LocationSelector/>
       {/* Add your destination-related components here */}
     </View>
   );
