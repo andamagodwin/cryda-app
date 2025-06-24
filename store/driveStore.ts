56a09702e77efbcb964a2ef02fe172a5751bd8d0
andamagodwin <andamagodwinezra@gmail.com>
2025-06-24 14:49:30 +0300

    Refactor Account and Rides components to manage user session and drives, integrate date-fns for date formatting, and enhance profile update functionality

--- store/driveStore.ts
+++ store/driveStore.ts
@@ -6,9 +6,16 @@ type Coordinates = {
   label: string;
 };
 
+type Location = {
+  latitude: number;
+  longitude: number;
+  label?: string; // Optional label for display purposes
+};
+
 
 
 type Drive = {
+  id: string;
   driver_name: string;
   car_type: string;
   number_plate: string;
@@ -17,22 +24,24 @@ type Drive = {
   start_location: Location;
   end_location: Location;
   departure_time: string;
-  created_at?: string;
+  created_at: string;
 };
 
 
 type DriveState = {
   startLocation: Coordinates | null;
   endLocation: Coordinates | null;
-  drives: Drive[];
+  drives: Drive[] | null;
   setStartLocation: (loc: Coordinates) => void;
   setEndLocation: (loc: Coordinates) => void;
+  setDrives: (drives: Drive[]) => void;
 };
 
 export const useDriveStore = create<DriveState>((set) => ({
   startLocation: null,
   endLocation: null,
-  drives: [],
+  drives: null,
   setStartLocation: (loc) => set({ startLocation: loc }),
   setEndLocation: (loc) => set({ endLocation: loc }),
+  setDrives: (drives) => set({ drives }),
 }));
